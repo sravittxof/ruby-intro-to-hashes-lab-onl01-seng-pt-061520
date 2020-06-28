59fee3af4ee9d2635b746be6ed439aeb6f5fd21a
Travis Fox <travis.fox90@gmail.com>
2020-06-28 23:43:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,10 +17,25 @@ def monopoly
 	    :pieces => "4",
 	    :names =>
 	    {
-	     },
+	      :reading_railroad => {
+	      },
+	      :pennsylvania_railroad => {
+	      },
+	      :b_and_o_railroad => {
+	      },
+	      :shortline => {
+	      }
+      }
+    }
+	      
+      }
+     },
       :rent_in_dollars =>
       {
-        :
+        :one_piece_owned => "25",
+        :two_pieces_owned => "50",
+        :three_pieces_owned => "100",
+        :four_pieces_owned => "200"
       }
     },
   }
